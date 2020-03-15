cc7247926e25da4c9c5898864a7b63c4d3e02d27
emirhankenter <emirhankenter@gmail.com>
2020-03-15 23:52:56 +0300

    demo completed

---
+++ StrategyGame/Assets/Scripts/ErrorController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scripts
+{
+    public class ErrorController : MonoBehaviour
+    {
+        private Text _errorText;
+        private bool _isTextShown;
+        private void Start()
+        {
+            ErrorEvents.Current.OnOutOfSpaceTriggerEnter += OutOfSpace;
+        }
+
+        private void OutOfSpace()
+        {
+            if (_isTextShown) return;
+            _errorText = gameObject.GetComponent<Text>();
+            _errorText.text = "Out of Space";
+            _errorText.enabled = true;
+            _isTextShown = true;
+            StartCoroutine(PopUpErrorMessage());
+
+        }
+
+        IEnumerator PopUpErrorMessage()
+        {
+            yield return new WaitForSeconds(2f);
+            _errorText.enabled = false;
+        }
+    }
+}
